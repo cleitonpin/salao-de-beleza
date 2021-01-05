2bca0e86dc39b5d827e657cb5fed2e2268b79585
LRezende <lucas.rezende.2000@gmail.com>
2021-01-05 09:20:19 -0300

    Create navbar

--- tuca_front/src/pages/Landing/index.jsx
+++ tuca_front/src/pages/Landing/index.jsx
@@ -1,11 +1,12 @@
 import React from 'react';
+import Navbar from '../../components/Navbar';
 
 
 const Landing = (props) => {
 
     return (
         <>
-            Oi
+            <Navbar />
         </>
     )
 }
