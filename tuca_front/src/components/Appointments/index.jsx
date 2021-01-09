7a474ca59ea04d77ca591fff7c01b7682250fced
LRezende <lucas.rezende.2000@gmail.com>
2021-01-09 15:44:43 -0300

    Add appointment component

---
+++ tuca_front/src/components/Appointments/index.jsx
@@ -0,0 +1,35 @@
+import React from 'react';
+
+import './styles.css';
+
+export default function Appointments() {
+    return (
+        <section className='section' id='agendamentos' style={{ marginTop: 50 }}>
+            <div className='title'>
+                <h2>
+                    <span>/</span>Agendar
+                </h2>
+            </div>
+            <div>
+                <form action="">
+                    <h2>Agende seu horário</h2>
+
+                    <div class="input">
+                        <input required type="text" />
+                        <label>Nome</label>
+                    </div>
+
+                    <div class="input">
+                        <input required type="text" />
+                        <label>Horário</label>
+                    </div>
+
+                    <div class="input">
+                        <input required type="text" />
+                        <label>Dia da Semana</label>
+                    </div>
+                </form>
+            </div>
+        </section>
+    );
+}
