4ba3156827f8f87196424a1afc633af2ae090c14
LRezende <lucas.rezende.2000@gmail.com>
2021-01-10 02:07:33 -0300

    Fix migrations

--- tuca_back/src/database/migrations/20210102182832_drop_column_inscricao_estadual_servicos.js
+++
@@ -1,7 +0,0 @@
-const Knex = require("knex");
-
-exports.up = (knex = Knex) => knex.schema.table('servicos', (t) => {
-    t.dropColumn('inscricao_estadual');
-});
-
-exports.down = (knex) => knex.schema.dropTable('servicos');
--- tuca_back/src/database/migrations/20210102183546_create_table_users.js
+++ tuca_back/src/database/migrations/20210102183546_create_table_users.js
@@ -2,7 +2,7 @@ const Knex = require("knex");
 
 exports.up = (knex = Knex) => knex.schema.createTable('users', (t) => {
     t.increments('id').primary();
-    t.interger('funcionario_id').unsigned();
+    t.integer('funcionario_id').unsigned();
     t.foreign('funcionario_id').references('funcionarios.id');
     t.string('usuario').notNullable();
     t.string('senha').notNullable();
